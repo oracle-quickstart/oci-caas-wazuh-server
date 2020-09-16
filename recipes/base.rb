66089a4b90eb57fa7252720071c165f47055d0bd
Jon DeCamp <jon.decamp@oracle.com>
2020-09-16 16:11:08 -0700

    revert cron change

--- recipes/base.rb
+++ recipes/base.rb
@@ -1,7 +1,6 @@
 cron 'chef_client' do
   action :create
-  minute '38'
-  hour '1'
+  minute '0,15,30,45'
   user 'root'
   home '/opt/oci-caas/chef'
   command 'chef-client -z --runlist wazuh_server -j /opt/oci-caas/chef/attributes.json'
