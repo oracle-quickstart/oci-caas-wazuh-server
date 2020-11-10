515dae5b6650e71689583be44430b449dc2a6d51
Jon DeCamp <jon.decamp@oracle.com>
2020-11-10 09:34:36 -0800

    added more file integrity monitors for archived logs

--- attributes/syscheck.rb
+++ attributes/syscheck.rb
@@ -3,31 +3,34 @@ default['ossec']['conf']['syscheck']['disabled'] = false
 default['ossec']['conf']['syscheck']['frequency'] = 43200
 default['ossec']['conf']['syscheck']['scan_on_start'] = true
 default['ossec']['conf']['syscheck']['auto_ignore'] = [
-    { '@frequency' => '10', '@timeframe' => '3600', 'content!' => false }
-] 
+  { '@frequency' => '10', '@timeframe' => '3600', 'content!' => false },
+]
+
 default['ossec']['conf']['syscheck']['ignore'] = [
-    '/etc/mtab',
-    '/etc/hosts.deny',
-    '/etc/mail/statistics',
-    '/etc/random-seed',
-    '/etc/random.seed',
-    '/etc/adjtime',
-    '/etc/httpd/logs',
-    '/etc/utmpx',
-    '/etc/wtmpx',
-    '/etc/cups/certs',
-    '/etc/dumpdates',
-    '/etc/svc/volatile',
-    '/sys/kernel/security',
-    '/sys/kernel/debug',
-    '/dev/core',
-    { '@type' => 'sregex', 'content!' => '^/proc' },
-    { '@type' => 'sregex', 'content!' => '.log$|.swp$'}
+  '/etc/mtab',
+  '/etc/hosts.deny',
+  '/etc/mail/statistics',
+  '/etc/random-seed',
+  '/etc/random.seed',
+  '/etc/adjtime',
+  '/etc/httpd/logs',
+  '/etc/utmpx',
+  '/etc/wtmpx',
+  '/etc/cups/certs',
+  '/etc/dumpdates',
+  '/etc/svc/volatile',
+  '/sys/kernel/security',
+  '/sys/kernel/debug',
+  '/dev/core',
+  { '@type' => 'sregex', 'content!' => '^/proc' },
+  { '@type' => 'sregex', 'content!' => '.log$|.swp$' },
 ]
 
 default['ossec']['conf']['syscheck']['directories'] = [
   { '@check_all' => true, 'content!' => '/etc,/usr/bin,/usr/sbin' },
-  { '@check_all' => true, 'content!' => '/bin,/sbin,/boot' }
+  { '@check_all' => true, 'content!' => '/bin,/sbin,/boot' },
+  { '@check_all' => true, 'content!' => '/var/ossec/logs/archives/*/*/*.sum,/var/ossec/logs/archives/*/*/*.log.gz' },
+  { '@check_all' => true, 'content!' => '/var/log/secure-*,/var/log/messages-*' },
 ]
 
 default['ossec']['conf']['syscheck']['nodiff'] = '/etc/ssl/private.key'
